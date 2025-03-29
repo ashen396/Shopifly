22abc46fe6bbaae7ab1ba4726344deb3d80b692e
ashen396 <ashenrenon396@gmail.com>
2025-03-29 16:29:32 +0530

    Initial Commit

---
+++ Shopifly/ShopiflyApp.swift
@@ -0,0 +1,18 @@
+//
+//  ShopiflyApp.swift
+//  Shopifly
+//
+//  Created by Mac on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ShopiflyApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
