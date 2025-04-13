e78cf16be52bc71b5879de5aa5a290b4c872b094
ashen396 <ashenrenon396@gmail.com>
2025-04-13 11:07:26 +0530

    Implemented Product Screen

---
+++ Shopifly/Features/Productscreen/ProductscreenView.swift
@@ -0,0 +1,142 @@
+//
+//  ProductscreenView.swift
+//  Shopifly
+//
+//  Created by Mac on 4/12/25.
+//
+
+import SwiftUI
+
+struct ProductscreenView: View {
+    var body: some View {
+        VStack{
+            LinearGradient(gradient: Gradient(colors: [Color(red: 0.44, green: 0.03, blue: 0.07, opacity: 1.00), Color(red: 0.84, green: 0.02, blue: 0.12, opacity: 1.00)]), startPoint: .top, endPoint: .bottom)
+                .frame(width: Constants.screenWidth, height: Constants.screenWidth / 4.4, alignment: .top)
+            ZStack{
+                Image("RedShoeLarge")
+                    .resizable()
+                    .frame(width: Constants.screenWidth / 1.5, height: Constants.screenWidth / 1.5, alignment: .top)
+                
+                ZStack{
+                    VStack{
+                        HStack{
+                            Text("SHOE MARKETPLACE")
+                                .font(.title)
+                                .fontWeight(.black)
+                                .foregroundColor(.white)
+                                .multilineTextAlignment(.center)
+                                .frame(width: 250, height: 100, alignment: .center)
+
+                        }
+                        
+                        HStack{
+                            Image(systemName: "chevron.left")
+                                .resizable()
+                                .frame(width: 15, height: 25, alignment: .center)
+                                .foregroundColor(.white)
+                                
+                            Text("Browse")
+                                .foregroundColor(.white)
+                            
+                            Spacer()
+                            
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 25.0)
+                                    .frame(width: 32, height: 32, alignment: .center)
+                                    .foregroundColor(.white)
+                                
+                                Image(systemName: "exclamationmark.circle")
+                                    .resizable()
+                                    .frame(width: 25, height: 25, alignment: .center)
+                                    .foregroundColor(.red)
+                            }
+                        }.padding(.horizontal, 25)
+                        .frame(width: Constants.screenWidth, height: 40, alignment: .leading)
+                    }
+                }.frame(width: Constants.screenWidth, height: Constants.screenWidth, alignment: .top)
+                .padding(.top, 40)
+                
+                
+            }.frame(width: Constants.screenWidth, height: Constants.screenWidth / 1.5, alignment: .center)
+            .background(Color(red: 0.81, green: 0.02, blue: 0.12, opacity: 1.00))
+            .padding(.top, -10)
+            
+            Text("Red Shoe")
+                .font(.title)
+                .fontWeight(.bold)
+                .frame(width: Constants.screenWidth, height: 20, alignment: .leading)
+                .padding(.top, 25)
+                .padding(.leading, 40)
+            
+            Text("Rs. 1199")
+                .font(.body)
+                .fontWeight(.regular)
+                .foregroundColor(Color(UIColor.systemGray))
+                .frame(width: Constants.screenWidth, height: 20, alignment: .leading)
+                .padding(.top, 10)
+                .padding(.leading, 40)
+            
+            Spacer()
+                .frame(width: Constants.screenWidth, height: Constants.spacingHeight, alignment: .center)
+            
+            // Ratings
+            VStack{
+                HStack{
+                    Image("ProfilePic1")
+                        .resizable()
+                        .frame(width: 60, height: 60, alignment: .topLeading)
+                    
+                    VStack{
+                        
+                        // Username
+                        HStack{
+                            Text("@JohnSmith")
+                                .foregroundColor(Color(UIColor.systemGray2))
+                        }.frame(width: Constants.screenWidth - 85, height: 20, alignment: .leading)
+                        
+                        // Comment
+                        VStack{
+                            Text("Lorem ipsum dolor sit amet, consectetur adispsicing elit...")
+                                .padding(.trailing, 50)
+                                .frame(width: Constants.screenWidth - 85, height: 50, alignment: .leading)
+                                .multilineTextAlignment(.leading)
+                        }
+                        
+                        
+                    }.frame(width: Constants.screenWidth - 85, height: 50, alignment: .topLeading)
+                    
+                }
+                
+                Spacer()
+                    .frame(width: Constants.screenWidth, height: 20, alignment: .center)
+                
+                HStack{
+                    Image("ReviewImage")
+                        .resizable()
+                        .frame(width: 48, height: 48, alignment: .leading)
+                    
+                    Image("ReviewImage")
+                        .resizable()
+                        .frame(width: 48, height: 48, alignment: .leading)
+                    
+                    Image("ReviewImage")
+                        .resizable()
+                        .frame(width: 48, height: 48, alignment: .leading)
+                }.padding(.leading, 40)
+                .frame(width: Constants.screenWidth - 85, height: 48, alignment: .leading)
+                
+            }.padding(.horizontal, 15)
+            .frame(width: Constants.screenWidth, height: 150, alignment: .topLeading)
+            
+            CustomButton(title: "Write a Review", foregroundColor: .white, backgroundColor: .blue)
+            
+        }.frame(width: Constants.screenWidth, height: UIScreen.main.bounds.height, alignment: .top)
+        .ignoresSafeArea()
+    }
+}
+
+struct ProductscreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductscreenView()
+    }
+}
